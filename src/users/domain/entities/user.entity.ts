164dfb40025d68a28263885f248ee8b847c2f9d6
Renan Moreira <renan.bcc@gmail.com>
2023-12-20 11:52:39 -0300

    adding users entity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class UserEntity {}
+export interface IUserProps {
+  name: string;
+  cpf: number;
+  email: string;
+  password: string;
+  perfil: string;
+  address: {
+    street: string;
+    city: string;
+    country: string;
+    zipcode: string;
+  };
+  active: boolean;
+  createAt: Date;
+  updateAt: Date;
+}
+
+export class UserEntity {
+  constructor(public readonly props: IUserProps) {
+    this.props.createAt = this.props.createAt ?? new Date();
+    this.props.updateAt = this.props.updateAt ?? new Date();
+    this.props.active = this.props.active ?? true;
+    this.props.perfil = this.props.perfil ?? 'SAS';
+  }
+}
