c49cc1a927c3f8d63694325aacb887993d9fba27
Renan Moreira <renan.bcc@gmail.com>
2023-12-20 14:52:17 -0300

    feat<UserEntity>: adding update methods

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -31,31 +31,86 @@ export class UserEntity extends Entity<IUserProps> {
     this.props.perfil = this.props.perfil ?? 'SAS';
   }
 
+  // NOTE Name
+  updateName(value: string): void {
+    this.name = value;
+  }
+
   get name() {
     return this.props.name;
+  }
+
+  private set name(value: string) {
+    this.name = value;
+  }
+
+  // NOTE CPF
+  updateCpf(value: number): void {
+    this.cpf = value;
   }
 
   get cpf() {
     return this.props.cpf;
   }
 
+  private set cpf(value: number) {
+    this.cpf = value;
+  }
+
+  // NOTE Email
+  updateEmail(value: string): void {
+    this.email = value;
+  }
+
   get email() {
     return this.props.email;
+  }
+
+  private set email(value: string) {
+    this.email = value;
+  }
+
+  // NOTE Address
+  updateAddress(value: object): void {
+    this.address = value;
   }
 
   get address() {
     return this.props.address;
   }
 
+  private set address(value: object) {
+    this.address = value;
+  }
+
+  // NOTE Active
+  upadateActive(value: boolean): void {
+    this.active = value;
+  }
+
   get active() {
     return this.props.active;
   }
 
+  private set active(value: boolean) {
+    this.active = value;
+  }
+
+  // NOTE createAt
   get createAt() {
     return this.props.createAt;
   }
 
+  // NOTE updateAt
+  upadateUpdateAt(value: Date): void {
+    this.updateAt = value;
+  }
+
   get updateAt() {
     return this.props.updateAt;
+  }
+
+  private set updateAt(value: Date) {
+    this.updateAt = value;
   }
 }
