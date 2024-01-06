a655e163b1683d11efc3bcb15907251150380bbb
Renan Moreira <renan.bcc@gmail.com>
2024-01-06 16:25:57 -0300

    feat: adding abstract class for validate entities

---
+++ src/shared/domain/validators/class-validator-fields.ts
@@ -0,0 +1,31 @@
+import { validateSync } from 'class-validator';
+import { FieldsErrors, IValidatorFields } from './validator-fields.interface';
+
+/**
+ * NOTE Classe abstrata que utiliza o class-validator para validar as propriedade de uma classe.
+ * @template PropsValidated - O tipo genérico representa as propriedades a serem validadas.
+ */
+export abstract class ClassValidatorFields<PropsValidated>
+  implements IValidatorFields<PropsValidated>
+{
+  // Armazena os erros, caso exista.
+  erros: FieldsErrors = null;
+  // Armazenas os dados validados.
+  validatedData: PropsValidated = null;
+  // Método para validar os dados utilizando class-validatos.
+  validate(data: any): boolean {
+    const errors = validateSync(data);
+
+    if (errors.length) {
+      this.erros = {};
+      for (const error of errors) {
+        const field = error.property;
+        this.erros[field] = Object.values(error.constraints);
+      }
+    } else {
+      this.validatedData = data;
+    }
+
+    return !errors.length;
+  }
+}
--- src/shared/domain/validators/validator-fields.interface.ts
+++ src/shared/domain/validators/validator-fields.interface.ts
@@ -1,7 +1,9 @@
-export type FieldsErrors = {
+// NOTE Define a estrutura de erros para os campos.
+export interface FieldsErrors {
   [key: string]: string[];
-};
+}
 
+// NOTE Define os métodos que devem ser implementados por classes de validação.
 export interface IValidatorFields<PropsValidated> {
   erros: FieldsErrors;
   validatedData: PropsValidated;
